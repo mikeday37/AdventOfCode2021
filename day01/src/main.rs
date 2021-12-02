7f59d21f06df2cb38ab19ac05fbf7a837366068c
mikeday37 <mikeday370146@gmail.com>
2021-12-02 18:26:36 -0500

    better day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,68 +1,25 @@
 use std::fs;
 
-// new to rust -- my approach is barbaric.  :)
-// I intend to clean this up after learning more about Rust's ownership, re: error[E0382]
-
 fn main() {
-    println!("-- start --");
-
-    part1();
-    part2();
-
-    println!("--  end  --");
-}
-
-fn part1(){
-
     let input = fs::read_to_string("../input/input-01-full.txt").expect("Problem reading input.");
     let depths: Vec<u32> = input.lines().map(|line| line.parse().unwrap()).collect();
-
-    let mut first: bool = true;
-    let mut increases: u32 = 0;
-    let mut previous: u32 = 0;
-
-    for depth in depths {
-        if !first && depth > previous {
-            increases += 1;
-        }
-        first = false;
-        previous = depth;
-    }
-
-    println!("part 1: increases = {}", increases);
+    println!("part 1: {}", count_increases_in_window(&depths, 1));
+    println!("part 2: {}", count_increases_in_window(&depths, 3));
 }
 
-fn part2(){
-
-    let input = fs::read_to_string("../input/input-01-full.txt").expect("Problem reading input.");
-    let depths: Vec<u32> = input.lines().map(|line| line.parse().unwrap()).collect();
-
-    let mut index: i32 = -1;
-
-    let mut increases: u32 = 0;
-
-    let mut prev: u32 = 0;
-    let mut prev2: u32 = 0;
-
-    let mut sum: u32 = 0;
-    let mut prevsum: u32 = 0;
-
-    for depth in depths {
-        index += 1;
-
-        if index >= 2 {
-            sum = depth + prev + prev2;
+fn count_increases_in_window(values: &[u32], window_size: usize) -> u32 {
+    let mut sum = 0;
+    let mut prev = 0;
+    let mut increases = 0;
+    for start in 0..values.len() {
+        sum += values[start];
+        if start >= window_size {
+            sum -= values[start - window_size];
+            if sum > prev {
+                increases += 1;
+            }
         }
-
-        if index >= 3 && sum > prevsum {
-            increases += 1;
-        }
-
-        prev2 = prev;
-        prev = depth;
-
-        prevsum = sum;
+        prev = sum;
     }
-
-    println!("part 2: increases = {}", increases);
+    increases
 }
